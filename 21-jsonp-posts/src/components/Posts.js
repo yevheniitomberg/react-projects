aae52558000218b89a356d8cd9ff96e4e7b1d6d9
Yevhenii Tomberg <yevhenii@tomberg.com>
2023-01-11 19:53:03 +0300

    posts proj

---
+++ 21-jsonp-posts/src/components/Posts.js
@@ -0,0 +1,38 @@
+import { useEffect, useState } from "react"
+import Post from "./Post"
+
+function Posts() {
+  const [posts, setPosts] = useState([])
+  const [error, setError] = useState("")
+  const [isLoading, setLoading] = useState(true)
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts")
+      .then((response) => response.json())
+      .then((json) => setPosts(json))
+      .catch((error) => setError(error.message))
+      .finally(() => setLoading(false))
+  }, [])
+
+  if (error) {
+    return <h1>Error: {error}</h1>
+  }
+
+  return (
+    <>
+      <h1>Posts</h1>
+      <hr />
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        posts &&
+        posts.map((post) => {
+          return <Post {...post} key={post.id} />
+        })
+      )}
+      <div></div>
+    </>
+  )
+}
+
+export default Posts
